90c7fa38844b109c0be7aaeda3f6604139cb4b86
Giulio Bottari <giulio.bottari@tvglobo.com.br>
2019-01-25 10:56:59 -0200

    Adds setup recipe.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='flask-logger',
+      version='0.1',
+      description='A simple logger for Flask applications.',
+      url='https://github.com/gbottari/flask-logger',
+      author='Giulio Bottari',
+      author_email='giuliobottari@gmail.com',
+      license='MIT',
+      packages=['logger'],
+      zip_safe=False)
